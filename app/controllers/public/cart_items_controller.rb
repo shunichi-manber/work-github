20d16a55f062dd1348666b6d29752f3f2261aee1
EC2 Default User <ec2-user@ip-172-31-47-189.ap-northeast-1.compute.internal>
2023-11-14 15:55:45 +0000

    [Add]カート機能追加

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,22 +1,35 @@
 class Public::CartItemsController < ApplicationController
   def index
     @cart_item = current_customer
-    @cart_items = CartItem.all
+    @cart_items = current_customer.cart_items
   end
 
   def create
-    @cart_item = CartItem.new(cart_item_params)
-    @cart_item.customer = current_customer
-    @cart_item.save
-    redirect_to cart_items_path
+    if CartItem.find_by(item_id: params[:cart_item][:item_id]).present?
+      cart_item = CartItem.find_by(item_id: params[:cart_item][:item_id])
+      cart_item.amount += params[:cart_item][:amount].to_i
+      cart_item.update(amount: cart_item.amount)
+      redirect_to cart_items_path
+    end
   end
-  
+
   def update
     @cart_item = CartItem.find(params[:id])
     @cart_item.update(cart_item_params)
     redirect_to cart_items_path(current_customer)
   end
-  
+
+  def destroy
+    cart_item = CartItem.find(params[:id])
+    cart_item.destroy
+    redirect_to cart_items_path(current_customer)
+  end
+
+  def destroy_all
+    CartItem.destroy_all
+    redirect_to cart_items_path(current_customer)
+  end
+
   private
 
   def cart_item_params
